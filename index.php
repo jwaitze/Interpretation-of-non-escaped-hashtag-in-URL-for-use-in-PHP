527bc34155d5f67f2bf3b0cc0483aabeec3e12a0
Jake Waitze <jake@waitze.net>
2017-09-17 23:02:55 -0400

    Fixed spacing

--- index.php
+++ index.php
@@ -12,14 +12,16 @@
 </html>
 
 <?php
-
 	$request_uri = $_SERVER['REQUEST_URI'];
 	$request_uri_parts = explode('?', $request_uri, 2);
+	
 	$url_path = $request_uri_parts[0];
 	$url_path_parts = explode('/', $url_path);
+	
 	$irc_network = $url_path_parts[1];
 	$irc_channel = urldecode($url_path_parts[2]);
 	$irc_nickname = $url_path_parts[3];
+	
 	echo 'Network: ' . $irc_network . '<br>';
 	echo 'Channel: ' . $irc_channel . '<br>';
 	echo 'Nickname: ' . $irc_nickname . '<br>';
