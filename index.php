628baf8a4e81b6c6242946748358bd6fbfbc316f
Jake Waitze <jake@waitze.net>
2017-09-17 23:52:15 -0400

    Updated code to fit PHP standards a bit more closely

--- index.php
+++ index.php
@@ -8,9 +8,6 @@
 				+ window.location.hash.split('?')[0]));
 		}
 	</script>
-</body>
-</html>
-
 <?php
 	$request_uri = $_SERVER['REQUEST_URI'];
 	$request_uri_parts = explode('?', $request_uri, 2);
@@ -21,8 +18,10 @@
 	$irc_network = $url_path_parts[1];
 	$irc_channel = urldecode($url_path_parts[2]);
 	$irc_nickname = $url_path_parts[3];
-	
-	echo 'Network: ' . $irc_network . '<br>';
-	echo 'Channel: ' . $irc_channel . '<br>';
-	echo 'Nickname: ' . $irc_nickname . '<br>';
 ?>
+	<div>Network: <?= $irc_network ?></div>
+	<div>Channel: <?= $irc_channel ?></div>
+	<div>Nickname: <?= $irc_nickname ?></div>
+</body>
+</html>
+
